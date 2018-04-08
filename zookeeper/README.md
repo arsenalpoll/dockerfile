98833edb444c5ab577d1331e4788413e7aa07149
arsenalpoll <s_yuxiao@163.com>
2018-04-08 10:35:45 +0800

    add zookeeper Dockerfile

---
+++ zookeeper/README.md
@@ -0,0 +1,57 @@
+zookeeper文档：
+变量：
+ENV tickTime=2000
+ENV dataDir=/var/lib/zookeeper/data
+ENV dataLogDir=/var/lib/zookeeper/logs
+ENV clientPort=2181
+ENV initLimit=20
+ENV syncLimit=10
+ENV maxClientCnxns=60
+
+单机：
+#useradd -s /sbin/nologin zookeeper -u 1015 && \
+chown -R  zookeeper:zookeeper /sunyard/middleware/zookeeper
+
+#docker run -d --restart=always \
+--name zookeeper \
+--log-driver=json-file --log-opt max-size=100k --log-opt max-file=5 \
+-e ZOO_CLUSTER=no \
+-p 2181:2181 -p 2888:2888 -p 3888:3888 \
+-v /sunyard/middleware/zookeeper:/var/lib/zookeeper  \
+docker.sunyard.com:5000/zookeeper:3.4.11
+
+
+
+集群：
+#useradd  -s /sbin/nologin  zookeeper -u 1015 && \
+chown -R  zookeeper:zookeeper /sunyard/middleware/zookeeper
+
+middleware1：
+#docker run -d --restart=always \
+--name zookeeper \
+--log-driver=json-file --log-opt max-size=100k --log-opt max-file=5 \
+-e ZOO_CLUSTER=yes \
+-p 2181:2181 -p 2888:2888 -p 3888:3888 \
+-v /sunyard/middleware/zookeeper:/var/lib/zookeeper  \
+docker.sunyard.com:5000/zookeeper:3.4.11  \
+zoo3.sunyard.com,zoo2.sunyard.com,zoo1.sunyard.com  3
+ 
+middleware2：
+#docker run -d --restart=always \
+--name zookeeper \
+-p 2181:2181 -p 2888:2888 -p 3888:3888 \
+-e ZOO_CLUSTER=yes \
+--log-driver=json-file --log-opt max-size=100k --log-opt max-file=5 \
+-v /sunyard/middleware/zookeeper:/var/lib/zookeeper  \
+docker.sunyard.com:5000/zookeeper:3.4.11  \
+zoo3.sunyard.com,zoo2.sunyard.com,zoo1.sunyard.com  2
+
+middleware3：
+#docker run -d --restart=always \
+--name zookeeper \
+-e ZOO_CLUSTER=yes \
+--log-driver=json-file --log-opt max-size=100k --log-opt max-file=5 \
+-p 2181:2181 -p 2888:2888 -p 3888:3888 \
+-v /sunyard/middleware/zookeeper:/var/lib/zookeeper  \
+docker.sunyard.com:5000/zookeeper:3.4.11  \
+zoo3.sunyard.com,zoo2.sunyard.com,zoo1.sunyard.com  1
---
+++ zookeeper/entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$ZOO_CLUSTER" == "yes" ]
+
+then
+export ZOOKEEPER_SERVERS=$1
+export ZOOKEEPER_ID=$2
+echo "$ZOOKEEPER_ID" | tee $dataDir/myid
+
+ZOOKEEPER_CONFIG=
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"tickTime=$tickTime"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"dataDir=$dataDir"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"clientPort=$clientPort"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"initLimit=$initLimit"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"syncLimit=$syncLimit"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"maxClientCnxns=$maxClientCnxns"
+
+IFS=', ' read -r -a ZOOKEEPER_SERVERS_ARRAY <<< "$ZOOKEEPER_SERVERS"
+export ZOOKEEPER_SERVERS_ARRAY=$ZOOKEEPER_SERVERS_ARRAY
+for index in "${!ZOOKEEPER_SERVERS_ARRAY[@]}"
+do
+    ZKID=$(($index+1))
+    ZKIP=${ZOOKEEPER_SERVERS_ARRAY[index]}
+    if [ $ZKID == $ZOOKEEPER_ID ]
+    then
+        # if IP's are used instead of hostnames, every ZooKeeper host has to specify itself as follows
+        ZKIP=0.0.0.0
+    fi
+    ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"server.$ZKID=$ZKIP:2888:3888"
+done
+
+echo "$ZOOKEEPER_CONFIG" | tee conf/zoo.cfg
+chown -R zookeeper:zookeeper /var/lib/zookeeper
+
+else
+
+ZOOKEEPER_CONFIG=
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"tickTime=$tickTime"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"dataDir=$dataDir"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"clientPort=$clientPort"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"initLimit=$initLimit"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"syncLimit=$syncLimit"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"maxClientCnxns=$maxClientCnxns"
+
+echo "$ZOOKEEPER_CONFIG" | tee conf/zoo.cfg
+chown -R zookeeper:zookeeper /var/lib/zookeeper
+fi
+
+# start the server:
+su zookeeper -s /bin/bash  -c "/usr/share/zookeeper/bin/zkServer.sh start-foreground | tee /var/lib/zookeeper/zookeeper.out"
